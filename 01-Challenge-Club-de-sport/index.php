4201436f76f90b05f2ef88eec864086c6b5faaba
Eva-Oclock <44224858+Eva-Oclock@users.noreply.github.com>
2020-03-28 10:39:23 +0100

    Initial commit

---
+++ 01-Challenge-Club-de-sport/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>O'clock United</title>
+    </head>
+    <body>
+
+        <h1>O'clock United Football Club</h1>
+        <h2>Section enfants (de 6 à 14 ans)</h2>
+
+        <?php
+        /*
+
+        1.1
+        Ajouter un formulaire HTML avec un INPUT "age" permettant d'entrer un entier, et un bouton de type "submit"
+
+        1.2
+        Récupérer la valeur de l'âge envoyé grâce au formulaire
+
+        1.3
+        écrire le script qui indique la catégorie selon $age:
+        "Trop jeune" si moins de 6 ans
+        "Poussin" de 6 à 7 ans
+        "Pupille" de 8 à 9 ans
+        "Minime" de 10 à 11 ans
+        "Cadet" après 12 ans
+        "Trop âgé" à partir de 15 ans
+
+        */
+        ?>
+
+    </body>
+</html>
---
+++ README.md
@@ -0,0 +1,8 @@
+# Challenges PHP
+
+On s'attaque à l'envoi de données grâce aux formulaires HTML et à leur récupération en PHP.
+
+Pour cela, 2 challenges !
+
+- [O'clock United Football Club](01-Challenge-Club-de-sport/README.md)
+- [O'ssurances](02-Challenge-Assurances/README.md)
